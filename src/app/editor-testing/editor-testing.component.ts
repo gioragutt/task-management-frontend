8d42d30d515ae1dda556fc5e2e784c0d1d50c47e
Giora Guttsait <giora111@gmail.com>
2019-07-29 23:54:02 +0300

    Some tryouts with the monaco editor 👌🏻

--- src/app/editor-testing/editor-testing.component.ts
+++ src/app/editor-testing/editor-testing.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MonacoOptions, MonacoEditorComponent } from '@materia-ui/ngx-monaco-editor';
+import { editor } from 'monaco-editor';
 
 @Component({
   selector: 'app-editor-testing',
   templateUrl: './editor-testing.component.html',
   styleUrls: ['./editor-testing.component.scss'],
 })
-export class EditorTestingComponent implements OnInit {
+export class EditorTestingComponent {
+  @ViewChild(MonacoEditorComponent, { static: false }) editor: MonacoEditorComponent;
 
-  constructor() { }
+  opts: editor.IEditorConstructionOptions;
+  themes = ['vs', 'vs-dark', 'hc-black'];
 
-  ngOnInit() {
+  editorOptions: MonacoOptions = {
+    theme: 'high-contrast-dark',
+    language: 'json',
+    minimap: 'false',
+  };
+
+  code = 'function x() {\nconsole.log("Hello world!");\n}';
+  originalCode = 'function x() { // TODO }';
+
+  onThemeChanged(event: any) {
+    this.changeEditorSetting('theme', event.target.value);
   }
 
+  onMinimapChanged(event: any) {
+    const minimap: editor.IEditorMinimapOptions = {
+      enabled: event.target.checked,
+    };
+    this.editor.editor.updateOptions({ minimap });
+  }
+
+  private changeEditorSetting<K extends keyof MonacoOptions>(setting: K, value: MonacoOptions[K]) {
+    this.editorOptions = {
+      ...this.editorOptions,
+      [setting]: value,
+    };
+  }
+
+  // https://github.com/fge/json-schema-avro
 }
--- src/app/editor-testing/editor-testing.module.ts
+++ src/app/editor-testing/editor-testing.module.ts
@@ -1,15 +1,17 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MonacoEditorModule } from '@materia-ui/ngx-monaco-editor';
 import { EditorTestingRoutingModule } from './editor-testing-routing.module';
 import { EditorTestingComponent } from './editor-testing.component';
-
 
 @NgModule({
   declarations: [EditorTestingComponent],
   imports: [
     CommonModule,
     EditorTestingRoutingModule,
+    MonacoEditorModule,
+    FormsModule,
   ],
 })
 export class EditorTestingModule { }
